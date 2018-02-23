34a00f69c1eb25cdc43bf13e034edeaa4679ccf1
Alexander Urizar <urizaraj@gmail.com>
2018-02-23 20:30:25 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -42,4 +42,15 @@ class Student
     student.save
     student
   end
+
+  def self.find_by_name(name)
+    sql = <<-SQL
+    SELECT *
+    FROM students
+    WHERE name = ?
+    SQL
+
+    row = DB[:conn].execute(sql, name).first
+    new_from_db(row)
+  end
 end
