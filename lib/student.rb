2198d52caf99a8d5fd3b7bc02dd0ce8413711583
Alexander Urizar <urizaraj@gmail.com>
2018-02-23 20:34:27 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -3,7 +3,7 @@ require_relative "../config/environment.rb"
 class Student
   attr_accessor :name, :grade
   attr_reader :id
-  
+
   def initialize(name, grade, id=nil)
     self.name = name
     self.grade = grade
