50746c8be1f5adfee86f25129eb4b2e7cc8da02a
Alexander Urizar <urizaraj@gmail.com>
2018-02-23 20:36:48 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -60,4 +60,14 @@ class Student
     row = DB[:conn].execute(sql, name).first
     new_from_db(row)
   end
+
+  def update
+    sql = <<-SQL
+    UPDATE students
+    SET name = ?, grade = ?
+    WHERE id = ?
+    SQL
+
+    DB[:conn].execute(sql, self.name, self.grade, self.id)
+  end
 end
